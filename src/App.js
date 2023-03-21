dcb1bdd9e0f0c258bd1d47a4dda4922b6f067030
hstefanova <hristiyana.stefanova@mentormate.com>
2023-03-21 21:26:31 +0200

    apply lazy loading

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from './pages/Home';
+// import PostPage, { loader as postLoader } from './pages/Post';
+import RootLayout from './pages/Root';
+
+// import gives you a promise
+const BlogPage = lazy(() => import('./pages/Blog'));
+const PostPage = lazy(() => import('./pages/Post'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'posts',
+        children: [
+          {
+            index: true,
+            element: (
+              <Suspense>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import('./pages/Blog').then((module) => module.loader()),
+          },
+          {
+            path: ':id',
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            // meta holds the params with the ID
+            loader: (meta) =>
+              import('./pages/Post').then((module) => module.loader(meta)),
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
